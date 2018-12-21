3c7929d4b524c03990f711f8139e20df063d8642
Maciej Migdal <maciej.migdal@contractors.roche.com>
2018-12-21 06:22:42 +0100

    Add function for checking allele number resolution

--- MiDAS/R/MiDAS_utils.R
+++ MiDAS/R/MiDAS_utils.R
@@ -15,7 +15,7 @@ checkAlleleFormat <- function(allele) {
     stop("Error: Allele have to be of type character.")
   }
   pattern <- ".*[*][0-9][0-9](:[0-9][0-9])*[NLSCAQ]{0,1}$"
-  is_correct <- stringi::stri_detect(pattern, allele)
+  is_correct <- stringi::stri_detect(allele, regex = pattern)
   return(is_correct)
 }
 
@@ -29,6 +29,13 @@ checkAlleleFormat <- function(allele) {
 #' allele <- c("A*01:01", "A*01:02")
 #' getAlleleResolution(allele)
 getAlleleResolution <- function(allele) {
+  if (! typeof(allele) == "character") {
+    stop("Error: Allele have to be of type character.")
+  }
+  if (! all(checkAlleleFormat(allele))) {
+    stop("Error: Allele have to be a valid HLA allele number.")
+  }
+  allele_resolution <- 2 * (stringi::stri_count(allele, fixed = ":") + 1)
   return(allele_resolution)
 }
 
