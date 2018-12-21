6646681ff678d753c7833473de8a508e9d6c79cd
Maciej Migdal <maciej.migdal@contractors.roche.com>
2018-12-21 08:36:26 +0100

    Call stri functions directly

--- MiDAS/R/MiDAS_utils.R
+++ MiDAS/R/MiDAS_utils.R
@@ -15,7 +15,7 @@ checkAlleleFormat <- function(allele) {
     stop("Error: Allele have to be of type character.")
   }
   pattern <- "^(.+[*]){0,1}[0-9][0-9](:[0-9][0-9]){0,3}[NLSCAQ]{0,1}$"
-  is_correct <- stringi::stri_detect(allele, regex = pattern)
+  is_correct <- stringi::stri_detect_regex(allele, pattern)
   return(is_correct)
 }
 
@@ -29,13 +29,10 @@ checkAlleleFormat <- function(allele) {
 #' allele <- c("A*01:01", "A*01:02")
 #' getAlleleResolution(allele)
 getAlleleResolution <- function(allele) {
-  if (! typeof(allele) == "character") {
-    stop("Error: Allele have to be of type character.")
-  }
   if (! all(checkAlleleFormat(allele))) {
     stop("Error: Allele have to be a valid HLA allele number.")
   }
-  allele_resolution <- 2 * (stringi::stri_count(allele, fixed = ":") + 1)
+  allele_resolution <- 2 * (stringi::stri_count_fixed(allele, ":") + 1)
   return(allele_resolution)
 }
 
@@ -48,5 +45,10 @@ getAlleleResolution <- function(allele) {
 #' @return Character vector containing reduced HLA allele numbers.
 reduceAlleleResolution <- function(allele,
                                    resolution=4) {
+  input_resolution <- getAlleleResolution(allele)
+  if (any(input_resolution < resolution)) {
+    stop("Error: Input resolution can't be lower than requested resolution.")
+  }
+
   return(reduced_allele)
 }
