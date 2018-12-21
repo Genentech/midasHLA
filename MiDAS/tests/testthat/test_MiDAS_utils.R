0ac99bb9fa7e9a70309d2b6242b8f00ff71eee8f
Maciej Migdal <maciej.migdal@contractors.roche.com>
2018-12-21 08:26:24 +0100

    fixed test for checkAlleleFormat; added test for getAlleleResolution

--- MiDAS/tests/testthat/test_MiDAS_utils.R
+++ MiDAS/tests/testthat/test_MiDAS_utils.R
@@ -3,9 +3,20 @@ context("HLA allele numbers")
 test_that("HLA allele numbers have proper format", {
   expect_equal(checkAlleleFormat(c("A*01", "A*01:24", "B*01:25:22",
                                    "C*05:24:55:54")
-                                 ), c(TRUE, TRUE, TRUE, TRUE)
+                                ), c(TRUE, TRUE, TRUE, TRUE)
                )
-  expect_equal(checkAlleleFormat(c("*01", "A*:22", "C*05:24:55:54")
-                                 ), c(FALSE, FALSE, FALSE)
+   expect_equal(checkAlleleFormat(c("01", "01:24", "01:25:22",
+                                   "05:24:55:54")
+                                ), c(TRUE, TRUE, TRUE, TRUE)
+               )
+  expect_equal(checkAlleleFormat(c("*01", "A*:22", "C*05:24:55:54:89")
+                                ), c(FALSE, FALSE, FALSE)
+              )
+})
+
+test_that("HLA allele resolution is number of sets of digits * 2", {
+  expect_equal(getAlleleResolution(c("A*01", "A*01:24", "B*01:25:22",
+                                     "C*05:24:55:54")
+                                  ), c(2, 4, 6, 8)
               )
 })
