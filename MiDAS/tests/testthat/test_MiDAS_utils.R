3188865bdd4af66a431c18d40f96b31e983968b5
Maciej Migdal <maciej.migdal@contractors.roche.com>
2018-12-22 19:12:39 +0100

    Some allele numbers might contain more than 2 digits in sets

--- MiDAS/tests/testthat/test_MiDAS_utils.R
+++ MiDAS/tests/testthat/test_MiDAS_utils.R
@@ -2,12 +2,12 @@ context("HLA allele numbers")
 
 test_that("HLA allele numbers have proper format", {
   expect_equal(checkAlleleFormat(c("A*01", "A*01:24", "B*01:25:22",
-                                   "C*05:24:55:54")
+                                   "C*050:24:55:54")
                ),
                c(TRUE, TRUE, TRUE, TRUE)
   )
   expect_equal(checkAlleleFormat(c("01", "01:24", "01:25:22",
-                                   "05:24:55:54")
+                                   "050:24:55:54")
                ),
                c(FALSE, FALSE, FALSE, FALSE)
   )
