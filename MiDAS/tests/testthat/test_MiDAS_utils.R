9a5e2dedfab6c7362bce5b0daff9c22ce46b573a
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2018-12-27 15:52:15 +0100

    Make expect_error more precise (HTSEQ-197)

--- MiDAS/tests/testthat/test_MiDAS_utils.R
+++ MiDAS/tests/testthat/test_MiDAS_utils.R
@@ -33,6 +33,6 @@ test_that("Reduced HLA allele have desired resoulution", {
                ),
                c("A*01", "A*01", "B*01", "C*05")
   )
-  expect_error(reduceAlleleResolution("C*05:24:55:54", resolution = "four"))
+  expect_error(reduceAlleleResolution("C*05:24:55:54", resolution = "four"),"resolution is not a count \\(a single positive integer\\)")
   expect_error(reduceAlleleResolution("word", resolution = 4))
 })
