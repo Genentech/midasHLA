c2ff7eb213020c65bb57535b1f0372a138fd8114
Maciej Migdal <maciej.migdal@contractors.roche.com>
2019-01-01 22:48:20 +0100

    download aa code table

--- download_extdata.R
+++ download_extdata.R
@@ -1,5 +1,7 @@
+library("dplyr")
 library("RCurl")
 library("XML")
+library("rvest")
 
 extdata_dir <- "inst/extdata/"
 hla_alignmnets_url <- "ftp://ftp.ebi.ac.uk/pub/databases/ipd/imgt/hla/alignments/"
@@ -14,3 +16,8 @@ for (aln in hla_alignmnets_db) {
     download.file(paste0(hla_alignmnets_url, aln), destfile = paste0(extdata_dir, aln))
   }
 }
+
+aa_code_url <- "http://www.fao.org/docrep/004/y2775e/y2775e0e.htm"
+aa_code_table <- html(aa_code_url) %>%
+  html_table(fill=TRUE)
+write.table(aa_code_table[[1]], file = "inst/extdata/aa_code.tsv", sep = "\t", col.names = FALSE, row.names = FALSE)
