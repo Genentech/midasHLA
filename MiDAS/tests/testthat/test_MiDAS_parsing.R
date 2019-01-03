783295c6bf17a56f8aa2f499cd921abca7c2994b
Maciej Migdal <maciej.migdal@contractors.roche.com>
2019-01-03 10:44:31 +0100

    Add test for parsers

---
+++ MiDAS/tests/testthat/test_MiDAS_parsing.R
@@ -0,0 +1,135 @@
+library(assertthat)
+library(testthat)
+
+context("HLA allele information parsing")
+
+
+test_that("HLA allele calls are read properly", {
+  expect_error(readHlaCalls("/path/to/non/existing/file"),
+               "Path '/path/to/non/existing/file' does not exist"
+  )
+
+  fake_calls <- data.frame(ID = c("Sample1", "Sample2", "Sample3"),
+                           A_1 = c("A*01", "A*02", "A*03"),
+                           A_2 = c("A*01", "B*02", "C*03")
+  )
+
+  fake_calls_no_id <- tempfile()
+  write.table(fake_calls[, c(2, 1, 3)],
+              file = fake_calls_no_id,
+              sep = "\t",
+              row.names = FALSE,
+              col.names = TRUE
+  )
+  expect_error(readHlaCalls(fake_calls_no_id),
+               "First column of input file should specify samples id"
+  )
+  unlink(fake_calls_no_id)
+
+  fake_calls_non_hla_numbers <- tempfile()
+  write.table(fake_calls[, c(1, 1, 3)],
+              file = fake_calls_non_hla_numbers,
+              sep = "\t",
+              row.names = FALSE,
+              col.names = TRUE
+  )
+  expect_error(readHlaCalls(fake_calls_non_hla_numbers),
+               "Values in input file doesn't follow HLA numbers specification"
+  )
+  unlink(fake_calls_non_hla_numbers)
+
+  fake_calls_non_uniq_genes <- tempfile()
+  write.table(fake_calls,
+              file = fake_calls_non_uniq_genes,
+              sep = "\t",
+              row.names = FALSE,
+              col.names = TRUE
+  )
+  expect_error(readHlaCalls(fake_calls_non_uniq_genes),
+               "Gene names in columns are not identical"
+  )
+  unlink(fake_calls_non_uniq_genes)
+
+  fake_calls_NA_col <- tempfile()
+  fake_calls[, 3] <- NA
+  write.table(fake_calls,
+              file = fake_calls_NA_col,
+              sep = "\t",
+              row.names = FALSE,
+              col.names = TRUE
+  )
+  expect_error(readHlaCalls(fake_calls_NA_col),
+               "One of the columns contains only NA")
+  unlink(fake_calls_NA_col)
+})
+
+test_that("HLA allele alignments are read properly", {
+  expect_error(readHlaAlignments("/path/to/non/existing/file"),
+               "Path '/path/to/non/existing/file' does not exist"
+  )
+
+  aln_file <- system.file("extdata/A_prot.txt", package = "MiDAS")
+  hla_alignments <- readHlaAlignments(aln_file, trim = FALSE)
+  fasta_file <- system.file("extdata/A_prot.fasta", package = "MiDAS")
+  fasta <- seqinr::read.alignment(fasta_file, format = "fasta")
+  fasta <- fasta$seq[[1]]
+  expect_equal(paste(hla_alignments[1, ], collapse = ""),
+               toupper(fasta)
+  ) # check sequence with sequence from fasta
+
+  hla_alignments_trim <- readHlaAlignments(aln_file, trim = TRUE)
+  n_trimmed <- ncol(hla_alignments) - ncol(hla_alignments_trim)
+  expect_equal(n_trimmed, 24) # check if sequence is trimmed properly
+
+  fake_aln <- readLines(aln_file)
+  fake_aln <- vapply(X = fake_aln,
+                     FUN = function(x) {
+                       number <- stri_split_regex(x, "\\s+")[[1]]
+                       ifelse(any(checkAlleleFormat(number)), "empty ", x)
+                     },
+                     FUN.VALUE = character(length = 1),
+                     USE.NAMES = FALSE
+  )
+  fake_aln_tmp <- tempfile()
+  writeLines(text = fake_aln, con = fake_aln_tmp)
+  expect_error(readHlaAlignments(fake_aln_tmp),
+               "input file contains no correct HLA alignments"
+  )
+  unlink(fake_aln_tmp)
+
+  fake_aln <- readLines(aln_file)
+  fake_aln <- vapply(X = fake_aln,
+                     FUN = function(x) {
+                       number <- stri_split_regex(x, "\\s+")[[1]]
+                       if(any(checkAlleleFormat(number))) {
+                         li <- length(number) - 1
+                         number[li] <- paste(sample(c("?", ">", "<", "#", "@", "!"),
+                                                    nchar(number[li]),
+                                                    replace = TRUE
+                                             ),
+                                             collapse = ""
+                                       )
+                         paste(number, collapse = " ")
+                       } else {
+                         x
+                       }
+                     },
+                     FUN.VALUE = character(length = 1),
+                     USE.NAMES = FALSE
+  )
+  fake_aln_tmp <- tempfile()
+  writeLines(text = fake_aln, con = fake_aln_tmp)
+  expect_error(readHlaAlignments(fake_aln_tmp),
+               "alignments lines contain non standard characters"
+  )
+  unlink(fake_aln_tmp)
+
+  fake_aln <- readLines(aln_file)
+  fake_aln[grepl("Prot", fake_aln)] <- "Prot"
+  fake_aln_tmp <- tempfile()
+  writeLines(text = fake_aln, con = fake_aln_tmp)
+  expect_error(readHlaAlignments(fake_aln_tmp),
+               "first_codon_idx is not a count \\(a single positive integer\\)"
+  )
+  unlink(fake_aln_tmp)
+})
