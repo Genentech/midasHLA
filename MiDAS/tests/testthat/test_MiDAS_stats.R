622aa5311fd0a31bd2bec8a797e754dd23fb7bc6
Maciej Migdal <maciej.migdal@contractors.roche.com>
2019-03-22 02:00:34 +0100

    Rewrite tests for hlaAssocModel

--- MiDAS/tests/testthat/test_MiDAS_stats.R
+++ MiDAS/tests/testthat/test_MiDAS_stats.R
@@ -143,98 +143,52 @@ test_that("HLA allele associations are analyzed properly", {
 })
 
 test_that("HLA statistical models are defined properly", {
-  expect_equal(hlaAssocModels(), c("coxph", "lm", "glm.logit", "glm.nb"))
-
   hla_calls_file <- system.file(
     "extdata", "HLAHD_output_example.txt", package = "MiDAS"
   )
   hla_calls <- readHlaCalls(hla_calls_file)
-  hla_counts <- hlaCallsToCounts(hla_calls)
   pheno_file <- system.file("extdata", "pheno_example.txt", package = "MiDAS")
   pheno <- read.table(pheno_file, header = TRUE)
   covar_file <- system.file("extdata", "covar_example.txt", package = "MiDAS")
   covar <- read.table(covar_file, header = TRUE)
-  data <- dplyr::left_join(hla_counts, pheno, by = "ID")
-  data <- dplyr::left_join(data, covar, by = "ID")
-  response <- colnames(pheno[, -1])
-  covariate <- colnames(covar[, -1])
+  hla_data <- prepareHlaData(hla_calls, pheno, covar)
 
-  fun <- hlaAssocModels(model = "coxph",
-                 response = response,
-                 covariate = covariate,
-                 data = data
+  fun <- hlaAssocModel(model = "glm",
+                       response = hla_data$response[2],
+                       variable = hla_data$covariate,
+                       data = hla_data$data,
+                       family = binomial(link = "logit")
   )
-  res <- fun("`A*01:01`")
   expect_equal(
-    as.character(res$call),
-    c("coxph", "as.formula(form)", "data")
+    fun,
+    glm(
+      OS_DIED ~ AGE + SEX,
+      data = hla_data$data ,
+      family = binomial(link = "logit")
+    )
   )
 
-  response <- colnames(pheno[, 3, drop = FALSE])
-  fun <- hlaAssocModels(model = "lm",
-                        response = response,
-                        covariate = covariate,
-                        data = data
-  )
-  res <- fun("`A*01:01`")
-  expect_equal(
-    as.character(res$call),
-    c("lm", "as.formula(form)", "data")
+  expect_error(hlaAssocModel(model = 1),
+               "model have to be a string \\(a length one character vector\\) or a function"
   )
 
-  fun <- hlaAssocModels(model = "glm.logit",
-                        response = response,
-                        covariate = covariate,
-                        data = data
-  )
-  res <- fun("`A*01:01`")
-  expect_equal(
-    as.character(res$call),
-    c("glm", "as.formula(form)", "binomial(link = \"logit\")", "data")
+  expect_error(hlaAssocModel(model = "l"),
+               "could not find function l"
   )
 
-  fun <- hlaAssocModels(model = "glm.nb",
-                        response = response,
-                        covariate = covariate,
-                        data = data
-  )
-  res <- fun("`A*01:01`")
-  expect_equal(
-    as.character(res$call)[-4],
-    c("glm.nb", "as.formula(form)", "data", "log")
+  expect_error(hlaAssocModel(model = "lm", response = 1),
+               "response have to be a string or formula"
   )
 
-  # check if null covariate is accepted
-  fun <- hlaAssocModels(model = "glm.nb",
-                        response = response,
-                        covariate = NULL,
-                        data = data
-  )
-  res <- fun("`A*01:01`")
-  expect_equal(
-    colnames(res$model),
-    c("OS_DIED", "A*01:01")
+  expect_error(hlaAssocModel(model = "lm",
+                             response = hla_data$response[2],
+                             variable = 1),
+               "variable have to be a character or formula"
   )
 
-  expect_error(hlaAssocModels(model = 1),
-               "model is not a string \\(a length one character vector\\)."
-  )
-
-  expect_error(hlaAssocModels(model = "lm", response = 1),
-               "response is not a character vector"
-  )
-
-  expect_error(hlaAssocModels(model = "lm", response = character()),
-               "response can not be empty"
-  )
-
-  expect_error(hlaAssocModels(model = "lm", response = response, covariate = 1),
-               "covariate have to be a character or NULL"
-  )
-
-  expect_error(hlaAssocModels(model = "lm",
-                              response = response,
-                              covariate = covariate,
+  expect_error(hlaAssocModel(model = "lm",
+                              response = hla_data$response[2],
+                              variable = hla_data$covariate,
                               data = 1),
                "data is not a data.frame"
   )
