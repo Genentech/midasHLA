bfa6694dc43bb5a131650449ecd5866122e9d6f0
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2019-08-19 21:09:59 +0200

    Run devtools::test as CI tests

--- rplatform/run_tests.sh
+++ rplatform/run_tests.sh
@@ -6,4 +6,8 @@ echo "Working directory: `pwd`"
 echo "Working directory contains: `ls | tr '\n' ' '`"
 
 # Define how you run tests below
-echo "Hello!"
+# exit when any command fails
+set -e
+
+echo ">>>>> RUNNING UNIT TESTS"
+Rscript -e "devtools::test(pkg = '/mnt/vol/MiDAS', stop_on_failure = TRUE)"
