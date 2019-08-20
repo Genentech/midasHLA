a8464951f940a02032c66197026400d7e1a16bec
Maciej Migdal <maciej.migdal@contractors.roche.com>
2019-08-20 21:34:31 +0200

    hanging futher devel until combined prepare / analyze function emerge

--- MiDAS/R/MiDAS_summarise.R
+++ MiDAS/R/MiDAS_summarise.R
@@ -24,9 +24,39 @@ df <- lapply(unique(names(varaa)), function(x) data.frame(aa = varaa[x], hla = x
 df <- do.call(rbind, df)
 ggplot(data = df, mapping = aes(x = reorder(hla, -count), y = count, fill = aa)) +
   geom_bar(stat = "identity", position = ) +
-  labs(title = "DPB1_AA_8", y = "count", x = "")
+  labs(title = "DPB1_AA_8", y = "count", x = "") +
+  theme_bw()
 
 # It should somehow come from this
 aa_variation <- hlaToAAVariation(hla_calls)
 
 # mby its better to wait with this function until the association test is implemented
+
+### typical workflow ###
+library("survival")
+hla_calls_file <- system.file("extdata", "HLAHD_output_example.txt", package = "MiDAS")
+hla_calls <- readHlaCalls(hla_calls_file)
+pheno_file <- system.file("extdata", "pheno_example.txt", package = "MiDAS")
+pheno <- read.table(pheno_file, header = TRUE, stringsAsFactors = FALSE)
+covar_file <- system.file("extdata", "covar_example.txt", package = "MiDAS")
+covar <- read.table(covar_file, header = TRUE, stringsAsFactors = FALSE)
+midas_data <- prepareMiDASData(hla_calls = hla_calls,
+                               pheno = pheno,
+                               covar = covar,
+                               analysis_type = "aa_level",
+                               inheritance_model = "additive"
+)
+
+object <- lm(OS ~ AGE + SEX, data = midas_data)
+res <- analyzeMiDASData(object, analysis_type = "aa_level", kable_output = FALSE)
+
+
+aa_matrix <- hlaToAAVariation(hla_calls)
+f <- function(midas_data,
+              aa_pos,
+              frequency_cutoff) {
+  aa_pos <- paste0(aa_pos, "_")
+  aa_df <- select(midas_data, starts_with(aa_pos))
+}
+
+
