b6eab6fc30d2e83b7505139530b2b308705b3037
Maciej Migdal <maciej.migdal@contractors.roche.com>
2020-02-15 15:20:08 +0100

    add function calculating Grantham distance based on hla_calls for selected genes

--- MiDAS/R/MiDAS_utils.R
+++ MiDAS/R/MiDAS_utils.R
@@ -1158,3 +1158,121 @@ distGrantham <- function(aa1, aa2) {
 
   return(d)
 }
+
+#' Calculate Grantham disttance between alleles
+#'
+#' \code{hlaCallsGranthamDistance} calculate distance between HLA alleles using
+#' distance of choice.
+#'
+#' The Grantham distance is calculated for pairs of alleles for HLA gene.
+#'
+#' In case of missing alleles \code{NA} are returned.
+#'
+#' Grantham distance is calculated only for class I HLA alleles. First
+#' exons forming the variable region in the peptide binding groove (i.e.,
+#' exon 2 and 3) are selected (positions 1-182 in IMGT/HLA alignments, however
+#' here we take 2-182 as many 1st positions are missing). Then all the alleles
+#' containing gaps, stop codons or indels are discarded. Finally distance is
+#' calculated for each pair.
+#'
+#' @inheritParams checkHlaCallsFormat
+#' @param genes Character vector spcifying genes for which allelic distance
+#'   should be calculated.
+#'
+#' @return Data frame of normalized Grantham distances between pairs of alleles
+#'   for each specified HLA gene. First column (\code{ID}) is the same as in
+#'   \code{hla_calls}, further columns are named as given by \code{genes} and
+#'   contain numeric values.
+#'
+#' @importFrom assertthat assert_that is.string noNA see_if
+#' @importFrom magrittr %>%
+#' @importFrom rlang warn
+#' @importFrom stats na.omit
+#'
+#' @examples
+#' file <- system.file("extdata", "HLAHD_output_example.txt", package = "MiDAS")
+#' hla_calls <- readHlaCalls(file)
+#' hlaCallsGranthamDistance(hla_calls, genes = "A")
+#'
+#' @export
+hlaCallsGranthamDistance <- function(hla_calls, genes = c("A", "B", "C")) {
+  assert_that(
+    checkHlaCallsFormat(hla_calls),
+    is.character(genes),
+    noNA(genes)
+  )
+
+  hla_calls_colnames <- colnames(hla_calls)
+  target_genes <- hla_calls_colnames %>% # find genes present in hla_calls
+    `[`(-1) %>% # discard ID column
+    gsub(pattern = "_[0-9]+", replacement = "") %>%
+    unique()
+  assert_that(
+    characterMatches(x = genes, choice = target_genes) # asset is buggy hlaAlleleDistance(hla_calls, genes = c("A", "B", "FA"))
+  )
+  target_genes <- genes
+
+  d <- list(ID = hla_calls[, 1, drop = TRUE])
+  for (gene in target_genes) {
+    if (! gene %in% c("A", "B", "C")) {
+      warn(sprintf("Grantham distance is calculated only for class I HLA alleles. Ommiting gene %s", gene))
+      next()
+    }
+
+    sel <- paste0(gene, c("_1", "_2"))
+    pairs <- hla_calls[, sel]
+    resolution <- getAlleleResolution(unlist(pairs)) %>%
+      na.omit()
+    assert_that( # this should become obsolete
+      see_if(
+        all(resolution == resolution[1]),
+        msg = sprintf("Allele resolutions for gene %s are not equal", gene)
+      )
+    )
+
+    # process alignment
+    alignment <- readHlaAlignments(gene = gene, resolution = resolution[1])
+    alignment <- procHlaAlignmentGrantham(alignment)
+
+    allele_numbers <- rownames(alignment)
+    d[[gene]] <- vapply(
+      X = 1:nrow(pairs),
+      FUN = function(i) {
+        allele1 <- pairs[i, 1]
+        allele2 <- pairs[i, 2]
+        if (allele1 %in% allele_numbers && allele2 %in% allele_numbers) {
+          aa1 <- alignment[allele1, ]
+          aa2 <- alignment[allele2, ]
+          distGrantham(aa1, aa2)
+        } else {
+          warn(
+            sprintf(
+              fmt = "Alleles %s could not be found in the alignmnet coercing to NA",
+              paste(allele1, allele1, sep = ", ")
+            )
+          )
+          as.numeric(NA)
+        }
+      },
+      FUN.VALUE = numeric(length = 1L)
+    )
+  }
+
+  hla_dist <- data.frame(d, stringsAsFactors = FALSE)
+
+  return(hla_dist)
+}
+
+#' Helper function processing alignment for Grantham distance calculations
+#'
+#' \code{procHlaAlignmentGrantham} apply processing to raw HLA alignments, so
+#' that grantham distance between alleles can be calculated. Processing includes
+#' extracting exons 1 and 2, masking indels, gaps and stop codons.
+#'
+#' @param alignment Matrix containing HLA alleles amino acids level alignment.
+#'
+procHlaAlignmentGrantham <- function(alignment) {
+  alignment <- alignment[, 2:182] # select exons 2 and 3 w/o 1st position as it is biased towards missing data
+  mask <- apply(alignment, 1, function(x) any(x == "" | x == "X" | x == ".")) # mask gaps, stop codons, indels
+  alignment <- alignment[! mask, ]
+}
