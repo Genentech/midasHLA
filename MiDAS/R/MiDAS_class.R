f1aa731e98e4e42f5b8c70ac623dfd2c350b4ad0
Maciej Migdal <maciej.migdal@contractors.roche.com>
2020-04-06 09:13:59 +0200

    create POC MiDAS as MultiAssayExperiment

--- MiDAS/R/MiDAS_class.R
+++ MiDAS/R/MiDAS_class.R
@@ -1,217 +1,239 @@
-#' MiDAS class definition
+#' MiDAS class to hold all data and meta data about one MiDAS analysis.
+#'
+#' The \link{\code{MiDAS}} class is a \link{\code{MultiAssayExperiment}} object
+#' containing all the data and metadata about a set of measurments and thier
+#' transformations required for MiDAS analysis.
+#'
+#' @slot metadata A list that must at least contain a \code{inheritance_model}
+#'   element.
+#'
+#' The MiDAS object must contain at least \code{hla_calls} (see
+#' \link{\code{readHlaCalls}}) or \code{kir_calls} (see
+#' \link{\code{readKirCalls}}) experiments and defined \code{inheritance_model}
+#' element of \code{metadata}.
 #'
 #' @importFrom methods setClass
 #' @importClassesFrom MultiAssayExperiment MultiAssayExperiment
 #'
 #' @export
-.MiDAS <- setClass(
+MiDAS <- setClass(
   Class = "MiDAS",
-  slots = c(inheritance_model = "character"),
   contains = "MultiAssayExperiment"
 )
 
-#' MiDAS class constructor
+#' Validity method for class MiDAS
 #'
-#' @importFrom MultiAssayExperiment MultiAssayExperiment
-#' @importFrom magrittr %>% %<>%
+#' @importFrom assertthat assert_that is.string
+#'
+setValidity(Class = "MiDAS", method = function(object) {
+  assert_that(
+    if (! is.null(getHlaCalls(midas))) {
+      checkHlaCallsFormat(getHlaCalls(midas))
+    } else { TRUE },
+    if (! is.null(getKirCalls(midas))) {
+      checkKirCallsFormat(getKirCalls(midas))
+    } else { TRUE },
+    is.string(getInheritanceModel(midas)),
+    stringMatches(
+      x = getInheritanceModel(midas),
+      choice = c("additive", "dominant", "recessive")
+    )
+  )
+
+  return(TRUE)
+})
+
+#' Helper crating object of class MiDAS
+#'
+#' @examples
+#' # read hla calls file
+#' hla_calls_file <- system.file("extdata", "HLAHD_output_example.txt", package = "MiDAS")
+#' hla_calls <- readHlaCalls(hla_calls_file)
+#'
+#' # read batch covariates
+#' pheno_file <- system.file("extdata", "pheno_example.txt", package = "MiDAS")
+#' pheno <- read.table(pheno_file, header = TRUE, stringsAsFactors = FALSE)
+#' covar_file <- system.file("extdata", "covar_example.txt", package = "MiDAS")
+#' covar <- read.table(covar_file, header = TRUE, stringsAsFactors = FALSE)
+#' colData <- left_join(pheno, covar, by ="ID")
+#' rownames(coldata) <- coldata$ID
+#'
+#' # create MiDAS object
+#' midas <- MiDAS(hla_calls = hla_calls,
+#'                colData = colData,
+#'                inheritance_model = "additive"
+#' )
+#'
+#'
+#'
+#' @importFrom assertthat assert_that
+#' @importFrom methods as
+#' @importFrom MultiAssayExperiment ExperimentList MultiAssayExperiment
 #'
 #' @export
 MiDAS <- function(hla_calls = NULL,
-                  kir_counts = NULL,
-                  colData = NULL,
+                  kir_calls = NULL,
+                  colData,
                   inheritance_model = c("additive", "dominant", "recesive"),
-                  analysis_type = c("hla_allele", "aa_level", "expression_level", "allele_g_group", "allele_supertype", "allele_group", "kir_genes", "hla_kir_interactions"),
+                  analysis_type = c("hla_allele", "aa_level", "expression_level",
+                                    "allele_g_group", "allele_supertype",
+                                    "allele_group", "kir_genes",
+                                    "hla_kir_interactions", "custom"),
                   ...) {
-
-  experiments <- list()
-
-  if ("hla_allele" %in% analysis_type) {
-    hla_allele <- hlaCallsToCounts(
-      hla_calls = hla_calls,
-      inheritance_model = inheritance_model
-    )
-
-    hla_allele <- t(hla_allele[, -1]) # TODO w/o ID column
-    colnames(hla_allele) <- hla_calls$ID # TODO
-    experiments[["hla_allele"]] <- hla_allele
-  }
-
-  if ("aa_level" %in% analysis_type) {
-    aa_level <- hlaToAAVariation(
-      hla_calls = hla_calls,
-      indels = TRUE,
-      unkchar = FALSE
-    ) %>%
-      aaVariationToCounts(inheritance_model = inheritance_model)
-
-    aa_level <- t(aa_level[, -1]) # TODO w/o ID column
-    colnames(aa_level) <- hla_calls$ID # TODO
-    experiments[["aa_level"]] <- hla_allele
-  }
-
-  if ("expression_level" %in% analysis_type) {
-    lib <- listMiDASDictionaries()
-    lib <- grep("expression", lib, value = TRUE)
-    expression_level <- Reduce(
-      f = function(...) left_join(..., by = "ID"),
-      x = lapply(lib, hlaToVariable, hla_calls = hla_calls, na.value = NA)
-    )
-
-    assert_that(
-      ncol(expression_level) > 1,
-      msg = "no expression levels were found for input hla_calls"
-    )
-
-    expression_level <- expression_level %>%
-      gather("expression", "value", -c("ID")) %>%
-      mutate(expression = gsub("_[12]", "", .data$expression)) %>%
-      group_by(!!! syms(c("ID", "expression"))) %>%
-      summarise_all(funs(sum)) %>%
-      spread(.data$expression, .data$value, sep = NULL)
-
-    expression_level <- t(expression_level[, -1]) # TODO w/o ID column
-    colnames(expression_level) <- hla_calls$ID # TODO
-    experiments[["expression_level"]] <- expression_level
-  }
-
-  if ("allele_g_group" %in% analysis_type) {
-    lib <- "allele_HLA_Ggroup"
-    allele_g_group <- hlaToVariable(hla_calls = hla_calls,
-                                    dictionary = lib,
-                                    na.value = 0
-    )
-
-    assert_that(
-      ncol(allele_g_group) > 1,
-      msg = "no allele could be assigned to G group for input hla_calls"
-    )
-
-    allele_g_group <- hlaCallsToCounts(
-      hla_calls = allele_g_group,
-      inheritance_model = inheritance_model,
-      check_hla_format = FALSE
-    )
-
-    allele_g_group <- t(allele_g_group[, -1]) # TODO w/o ID column
-    colnames(allele_g_group) <- hla_calls$ID # TODO
-    experiments[["allele_g_group"]] <- allele_g_group
-  }
-
-  if ("allele_supertype" %in% analysis_type) {
-    lib <- "allele_HLA_supertype"
-    allele_supertype <- hlaToVariable(hla_calls = hla_calls,
-                                      dictionary = lib,
-                                      na.value = 0
-    )
-
-    assert_that(
-      ncol(allele_supertype) > 1,
-      msg = "no allele could be assigned to supertype for input hla_calls"
-    )
-
-    allele_supertype <- hlaCallsToCounts(
-      hla_calls = allele_supertype,
-      inheritance_model = inheritance_model,
-      check_hla_format = FALSE
-    ) %>%
-      select(-"Unclassified")
-
-    allele_supertype <- t(allele_supertype[, -1]) # TODO w/o ID column
-    colnames(allele_supertype) <- hla_calls$ID # TODO
-    experiments[["allele_supertype"]] <- allele_supertype # TODO w/o ID column
-  }
-
-  if ("allele_group" %in% analysis_type) {
-    lib <- c(
-      "allele_HLA-B_Bw",
-      "allele_HLA_Bw4+A23+A24+A32",
-      "allele_HLA-C_C1-2"
-    )
-    allele_group <- Reduce(
-      f = function(...) left_join(..., by = "ID"),
-      x = lapply(lib, hlaToVariable, hla_calls = hla_calls, na.value = 0)
-    )
-
-    assert_that(
-      ncol(allele_group) > 1,
-      msg = "no allele could be assigned to allele groups for input hla_calls"
-    )
-
-    allele_group <- hlaCallsToCounts(
-      hla_calls = allele_group,
-      inheritance_model = inheritance_model,
-      check_hla_format = FALSE
-    )
-
-    allele_group <- t(allele_group[, -1]) # TODO w/o ID column
-    colnames(allele_group) <- hla_calls$ID # TODO
-    experiments[["allele_group"]] <- allele_group
-  }
-
-  if ("kir_genes" %in% analysis_type) {
-    assert_that(
-      ! missing(kir_counts),
-      msg = "\"kir_genes\" analysis type requires kir_counts argument to be specified"
-    )
-
-    kir_counts <- t(kir_counts[, -1]) # TODO w/o ID column
-    colnames(kir_counts) <- kir_counts$ID # TODO
-    experiments[["kir_genes"]] <- kir_counts
-  }
-
-  if ("hla_kir_interactions" %in% analysis_type) {
-    assert_that(
-      ! missing(kir_counts),
-      msg = "\"hla_kir_interactions\" analysis type requires kir_counts argument to be specified"
-    )
-
-    hla_kir_interactions <- getHlaKirInteractions(
-      hla_calls = hla_calls,
-      kir_counts = kir_counts
-    )
-
-    hla_kir_interactions <- t(hla_kir_interactions[, -1]) # TODO w/o ID column
-    colnames(hla_kir_interactions) <- hla_kir_interactions$ID # TODO
-    experiments[["hla_kir_interactions"]] <- hla_kir_interactions
-  }
-
-  midas <- MultiAssayExperiment(
-    experiments = experiments,
-    colData = colData
+  inheritance_model_choice <- eval(formals()[["inheritance_model"]])
+  analysis_type_choice <- eval(formals()[["analysis_type"]])
+  assert_that(
+    if (! is.null(hla_calls)) {
+      checkHlaCallsFormat(hla_calls)
+    } else { TRUE },
+    if (! is.null(kir_calls)) {
+      checkKirCallsFormat(kir_calls)
+    } else { TRUE },
+    is.data.frame(colData),
+    is.string(inheritance_model),
+    stringMatches(inheritance_model, inheritance_model_choice),
+    is.character(analysis_type),
+    characterMatches(analysis_type, analysis_type_choice)
   )
 
-  midas <- .MiDAS(midas, inheritance_model = inheritance_model)
+  dot.args <- if (...length()) {
+    as.list(...)
+  } else {
+    list()
+  }
 
-  return(midas)
+  experiments <- ExperimentList()
+  for (at in analysis_type) {
+    fun <- paste0("prepareMiDAS_", at)
+    args <- list(
+      hla_calls = hla_calls,
+      kir_calls = kir_calls,
+      inheritance_model = inheritance_model
+    )
+    args <- c(args, dot.args)
+    experiment <- do.call(
+      what = fun,
+      args = args
+    )
+    experiments[[at]] <- experiment
+  }
+
+  metadata <- list(
+    hla_calls = hla_calls,
+    kir_calls = kir_calls,
+    inheritance_model = inheritance_model
+  )
+
+  mae <- MultiAssayExperiment(
+    experiments = as(experiments, "ExperimentList"),
+    colData = as(colData, "DataFrame"),
+    metadata = metadata
+  )
+
+  class(mae) <- structure("MiDAS", package = "MiDAS")
+
+  return(mae)
+}
+
+#' @name getInheritanceModel
+#'
+#' @title Extract inheritance model from MiDAS  object.
+#'
+#' @param object \link{\code{MiDAS}} object
+#'
+#' @return String giving name of object's inheritance model.
+#'
+#' @export
+setGeneric(
+  name = "getInheritanceModel",
+  def = function(object) standardGeneric("getInheritanceModel")
+)
+
+#' @rdname getInheritanceModel
+#'
+#' @importFrom S4Vectors metadata
+#'
+setMethod(
+  f = "getInheritanceModel",
+  signature = "MiDAS",
+  definition = function (object) metadata(object)$inheritance_model
+)
+
+#' @name getHlaCalls
+#'
+#' @title Extract HLA calls from MiDAS  object.
+#'
+#' @param object \link{\code{MiDAS}} object
+#'
+#' @return String giving name of object's inheritance model.
+#'
+#' @export
+setGeneric(
+  name = "getHlaCalls",
+  def = function(object) standardGeneric("getHlaCalls")
+)
+
+#' @rdname getHlaCalls
+#'
+#' @importFrom S4Vectors metadata
+#'
+setMethod(
+  f = "getHlaCalls",
+  signature = "MiDAS",
+  definition = function (object) metadata(object)$hla_calls
+)
+
+#' @name getKirCalls
+#'
+#' @title Extract KIR calls from MiDAS  object.
+#'
+#' @param a
+#'
+#' @return String giving name of object's inheritance model.
+#'
+#' @export
+setGeneric(
+  name = "getKirCalls",
+  def = function(object) standardGeneric("getKirCalls")
+)
+
+#' @rdname getKirCalls
+#'
+#' @importFrom S4Vectors metadata
+#'
+setMethod(
+  f = "getKirCalls",
+  signature = "MiDAS",
+  definition = function (object) metadata(object)$kir_calls
+)
+
+#' Prepare MiDAS data on HLA allele level
+#'
+#' @param hla_calls Data frame
+#' @param inheritance_model String
+#' @param ... Not used
+#'
+#' @return Matrix
+#'
+prepareMiDAS_hla_allele <- function(hla_calls, inheritance_model, ...) {
+  hla_allele <- hlaCallsToCounts(hla_calls = hla_calls,
+                                 inheritance_model = inheritance_model)
+  hla_allele <- t(hla_allele[, -1]) # TODO w/o ID column
+  colnames(hla_allele) <- hla_calls$ID # TODO
+
+  return(hla_allele)
 }
 
 #' Transform MiDAS to wide format data.frame
 #'
 #' @importFrom tidyr spread
+#' @importFrom dplyr mutate
 #'
 midasToWide <- function(object) {
   longFormat(object, colDataCols = TRUE) %>%
     as.data.frame() %>%
     subset(select = -assay) %>%
     subset(select = -colname) %>%
-    tidyr::spread(key = "rowname", value = "value") %>%
+    spread(key = "rowname", value = "value") %>%
     mutate(term = 1)
 }
-
-library("survival")
-hla_calls_file <- system.file("extdata", "HLAHD_output_example.txt", package = "MiDAS")
-hla_calls <- readHlaCalls(hla_calls_file)
-pheno_file <- system.file("extdata", "pheno_example.txt", package = "MiDAS")
-pheno <- read.table(pheno_file, header = TRUE, stringsAsFactors = FALSE)
-covar_file <- system.file("extdata", "covar_example.txt", package = "MiDAS")
-covar <- read.table(covar_file, header = TRUE, stringsAsFactors = FALSE)
-coldata <- left_join(pheno, covar, by ="ID")
-rownames(coldata) <- coldata$ID
-midas <- MiDAS(hla_calls = hla_calls,
-                           colData = coldata,
-                           analysis_type = "hla_allele",
-                           inheritance_model = "additive"
-)
-midas_data <- midasToWide(midas)
-object <- coxph(Surv(OS, OS_DIED) ~ AGE + SEX + term, data = midas_data)
-runMiDAS(object, analysis_type = "hla_allele", variables = rownames(midas[[1]]))
