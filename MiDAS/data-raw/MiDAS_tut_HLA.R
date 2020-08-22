5debb8c984d368c8c1ac92f2d1a7015aad2ab107
Maciej Migdal <maciej.migdal@contractors.roche.com>
2020-08-22 23:37:43 +0200

    reduce MiDAS_tut_HLA to 4 digits

--- MiDAS/data-raw/MiDAS_tut_HLA.R
+++ MiDAS/data-raw/MiDAS_tut_HLA.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env R
 devtools::load_all()
 
-MiDAS_tut_HLA <- readHlaCalls("inst/extdata/MiDAS_tut_HLA.txt", resolution = 8)
+MiDAS_tut_HLA <- readHlaCalls("inst/extdata/MiDAS_tut_HLA.txt", resolution = 4)
 
 usethis::use_data(MiDAS_tut_HLA, overwrite = TRUE)
